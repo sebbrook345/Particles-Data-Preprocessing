9d35975ccf572ad2b04f9653d58e1dac8b8371e3
Sebastian Brockly <sebastian.brockly@innobox.de>
2021-01-14 01:04:01 +0100

    implement parsing of csv

--- index.js
+++ index.js
@@ -1,127 +1,41 @@
-/**
- * Data preprocessing
- * Tasks:
- *  - Filter duplicates
- *  - Interpolating
- *  - Transform from 200 Hz to 50 Hz
- *  - Enrich data: 
- *     - force magnitude
- *     - moving average
- *     - variance (window, overall)
- */
-
-const parse = require('csv-parse')
 const fs = require('fs')
+const parse = require('./parse.js')
+const hist = require('./histogram.js')
 
-// read stream | reading file
-const input = fs.createReadStream('../__DATA__/sensor.tmp')
-
-const output = []
-const dt = 20
-let step = 1
-let curr = []
-
-// csv parser
-const parser = parse({
-    delimiter: ';',
-    from_line: 3
-    // columns: true
-})
-
-const isEqual = (a, b, eps = 0.001) => {
-    return Math.abs(a - b) < eps
+const getInputs = (str) => {
+    if (str.includes('[') && str.includes(']')) {
+        return str.replace('[', '').replace(']', '').split(',').map(s => s.trim())
+    }
+    else {
+        return [str]
+    }
 }
 
-const interpolate = (values) => {
-    const start = values[0]
-    const end = values[values.length - 1]
-    const inner = values.slice(1, values.length - 1)
-    const v = [
-        end[0] - start[0],
-        end[1] - start[1],
-        end[2] - start[2]
-    ]
-    return inner.map((_, index) => [
-        v[0] * ((index + 1) / (inner.length + 1)),
-        v[1] * ((index + 1) / (inner.length + 1)),
-        v[2] * ((index + 1) / (inner.length + 1))
-    ])
+const getIO = () => {
+    const inputIdx = process.argv.indexOf('--input')
+    const outputIdx = process.argv.indexOf('--output')
+
+    return {
+        input: getInputs(process.argv[inputIdx + 1]),
+        output: process.argv[outputIdx + 1]
+    }
 }
 
-// combinea
-const sensor = input.pipe(parser)
-
-let lineIdx = 0
-let fileIdx = 0
-sensor.on('data', (data) => {
-    const r = data.slice(0, data.length - 1).map(Number)
-    if (r[3] < step * dt) {
-        if (curr.length > 0) {
-            if (!isEqual(curr[curr.length - 1][0], r[0])) {
-                curr.push(r)
-            }
-        } else {
-            curr.push(r)
-        }
-    } else {
-        const out = curr.reduce(
-            (acc, val) => {
-                acc[0] += val[0]
-                acc[1] += val[1]
-                acc[2] += val[2]
-                return acc
-            }, [0, 0, 0, 0]
-        ).map(e => Math.round(e / curr.length * 1000000) / 1000000)
-        curr = []
-        out[3] = step * dt
-        output[lineIdx] = out
-        lineIdx += 1
-        step += 1
+const run = async () => {
+    const datasets = await parse(getIO().input)
+    const data = datasets.flat(1)
+    const histogram = hist(data.map(sample => sample.slice(0, 4)))
+    const meta = {
+        sampleSize: data.length
     }
+    
+    try {
+        fs.mkdirSync(getIO().outputFilePath)
+    } catch (e) {}
 
-    if (lineIdx === 1000000) {
-        sensor.pause()
-        console.log('end')
-        const nulls = []
-        for (let i = 1; i < output.length; i++) {
-            // console.log(output[i][0])
-            if (isNaN(output[i][0])) {
-                nulls.push(i)
-            }
-        }
+    fs.writeFileSync(`${getIO().output}/historgram.json`, JSON.stringify(histogram, null, 2))
+    fs.writeFileSync(`${getIO().output}/samples.json`, JSON.stringify(data, null, 2))
+    fs.writeFileSync(`${getIO().output}/meta.json`, JSON.stringify(meta, null, 2))
+}
 
-        const pairs = []
-
-        const currentPair = []
-        nulls.forEach(n => {
-            if (currentPair.length === 0) {
-                currentPair[0] = n
-                currentPair[1] = n
-            } else if (n - currentPair[1] === 1) {
-                currentPair[1] = n
-            } else {
-                pairs.push(currentPair.slice())
-                currentPair[0] = n
-                currentPair[1] = n
-            }
-        })
-
-        pairs.forEach(pair => console.log(interpolate([output[pair[0] - 1], ...output.slice(pair[0], pair[1] + 1), output[pair[1] + 1]])))
-        // sensor.resume()
-        process.exit()
-    }
-})
-
-sensor.on('end', () => {
-    // fs.writeFileSync(`${fileIdx}.csv`, JSON.stringify(output.slice(0, lineIdx), null, 2), { encoding: 'utf-8' })
-    console.log('end')
-    const nulls = []
-    for (let i = 1; i < output.length; i++) {
-        // console.log(output[i][0])
-        if (isNaN(output[i][0])) {
-            nulls.push(i)
-        }
-    }
-
-
-})
+run()
---
+++ parse.js
@@ -0,0 +1,168 @@
+/**
+ * Data preprocessing
+ * Tasks:
+ *  - Filter duplicates
+ *  - Interpolating
+ *  - Transform from 200 Hz to 50 Hz
+ *  - Enrich data: 
+ *     - force magnitude
+ *     - moving average
+ *     - variance (window, overall)
+ * 
+ * TODOs:
+ *   - Filter duplicates! (first step)
+ */
+
+const parse = require('csv-parse')
+const fs = require('fs')
+
+const get = (inputFilePath) => new Promise((resolve, reject) => {
+    // read stream | reading file
+    const input = fs.createReadStream(inputFilePath)
+    
+    const output = []
+    const dt = 20
+    let step = 1
+    let curr = []
+    
+    // csv parser
+    const parser = parse({
+        delimiter: ';',
+        from_line: 3
+        // columns: true
+    })
+    
+    const isEqual = (a, b, eps = 0.001) => {
+        return Math.abs(a - b) < eps
+    }
+    
+    const interpolate = (from, to, steps) => {
+        
+        // const start = values[0]
+        // const end = values[values.length - 1]
+        // const inner = values.slice(1, values.length - 1)
+        const v = [
+            to[0] - from[0],
+            to[1] - from[1],
+            to[2] - from[2],
+            to[3] - from[3]
+        ]
+    
+        const values = []
+    
+        for (let i = 0; i < steps; i++) {
+            values.push([
+                from[0] + (v[0] * ((i + 1) / (steps + 1))),
+                from[1] + (v[1] * ((i + 1) / (steps + 1))),
+                from[2] + (v[2] * ((i + 1) / (steps + 1))),
+                from[3] + (v[3] * ((i + 1) / (steps + 1)))
+            ])
+        }
+    
+        return values
+    }
+    
+    // combinea
+    const sensor = input.pipe(parser)
+    
+    const isEqualSample = (s1, s2) => {
+        return s1.slice(0, 3).every((_, i) => s2 && isEqual(s1[i], s2[i]))
+    }
+    
+    const samples = []
+    
+    let processed = 0
+    const empties = []
+    const maxima = [0, 0, 0, 0]
+    const transform = samples => {
+        const output = []
+        let temp = []
+        let index = 1
+        samples.forEach(sample => {
+            if (sample[3] < index * dt) {
+                temp.push(sample)
+            } else {
+                const next = temp.reduce(
+                    (acc, val) => {
+                        return [
+                            acc[0] + val[0],
+                            acc[1] + val[1],
+                            acc[2] + val[2],
+                            acc[3] + Math.sqrt(val[0] * val[0] + val[1] * val[1] + val[2] * val[2])
+                        ]
+                    }, [0, 0, 0, 0]
+                ).map(e => e / temp.length)
+    
+                // add to histogram
+    
+                next.forEach((p, i) => {
+                    if (Math.abs(p) > maxima[i]) {
+                        maxima[i] = Math.abs(p)
+                    }
+                })
+                next[4] = index * dt
+                output.push(next)
+                index += 1
+    
+                while (index * dt < sample[3]) {
+                    output.push([null, null, null, null, index * dt])
+                    empties.push(index)
+                    index += 1
+                }
+    
+                temp = [sample]
+            }
+        })
+    
+        return output
+    }
+    
+    const empty = (transformed) => {
+        const reduced = empties.reduce(
+            (acc, val) => {
+                if (val - acc.aux[acc.aux.length - 1] === 1) {
+                    acc.aux.push(val)
+                } else {
+                    acc.values.push(acc.aux)
+                    acc.aux = [val]
+                }
+                return acc
+            }, {
+            aux: [],
+            values: []
+        }
+        )
+    
+        return {
+            indices: reduced.values.slice(1).map(v => v.map(e => e - 1)),
+            interpolated: reduced.values.slice(1).map(
+                v => interpolate(transformed[v[0] - 2], transformed[v[v.length - 1]], v.length)
+            )
+        }
+    }
+    
+    sensor.on('data', data => {
+        const sample = data.slice(0, data.length - 1).map(Number)
+        if (!isEqualSample(sample, samples[samples.length - 1])) {
+            samples.push(sample)
+        }
+    })
+    
+    sensor.on('end', data => {
+        // fs.writeFileSync('raw.csv', JSON.stringify(samples, null, 2))
+        const transformed = transform(samples)
+        const { indices, interpolated } = empty(transformed)
+        indices.forEach((indexPairs, i) => {
+            indexPairs.forEach((index, j) => {
+                transformed[index] = interpolated[i][j]
+                transformed[index][4] = (index + 1) * dt
+            })
+        })
+        
+        resolve(transformed.map(t => t.slice(0, 4)))
+        // fs.writeFileSync(getIO().outputFilePath, JSON.stringify(transformed, null, 2))
+        // fs.writeFileSync('empties.csv', JSON.stringify(empty(transformed), null, 2))
+    }) 
+})
+
+module.exports = (inputFilePaths) => Promise.all(inputFilePaths.map(path => get(path)))
